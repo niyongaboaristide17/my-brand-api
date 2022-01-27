715ad597e1e134b61ed79bf83df91e0c2581724e
niyongaboaristide17 <49604980+niyongaboaristide17@users.noreply.github.com>
2022-01-27 16:18:22 +0200

    bug: article model images change to image

--- src/models/article.js
+++ src/models/article.js
@@ -4,7 +4,7 @@ const articleSchema = mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    images: String,
+    image: String,
     likes: { type: Number, default: 0 },
     created_on: { type: Date, default: Date.now }
 })
