b7eb494ee4e0369616e393036b8cc8930305d9a5
niyongaboaristide17 <niyongaboaristide17@gmail.com>
2022-01-31 15:46:37 +0200

    ft:article-validation validate article

--- src/routes/api/articleRoutes.js
+++ src/routes/api/articleRoutes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { ArticleController } from '../../controllers/articleController'
 import { fileFilter } from '../../helpers/fileFilter.js';
 import multer from 'multer'
+import { articleValidation } from '../../validations/articleValidation/article.validations.js';
 
 
 const route = express.Router()
@@ -11,7 +12,7 @@ const upload = multer({storage: storageFile, file: fileFilter})
 
 route.get('/',new ArticleController().getAllArticles)
 
-route.post('/',upload.single('image'), new ArticleController().createArticle)
+route.post('/', upload.single('image'), articleValidation, new ArticleController().createArticle)
 
 route.get('/:id', new ArticleController().getArticle)
 
---
+++ src/validations/articleValidation/article.schema.js
@@ -0,0 +1,8 @@
+
+import joi from 'joi'
+
+export const articleSchema = joi.object({
+    title: joi.string().max(1000).required(),
+    content: joi.string().required(),
+    image: joi.required()
+})
