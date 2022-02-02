aab533559e3e16e8d57622c66b9d54fe709ee3ab
niyongaboaristide17 <niyongaboaristide17@gmail.com>
2022-02-02 16:37:01 +0200

    bug:custom message added

--- src/validations/articleValidation/article.schema.js
+++ src/validations/articleValidation/article.schema.js
@@ -2,9 +2,16 @@
 import joi from 'joi'
 
 export const articleSchema = joi.object({
-    title: joi.string().max(1000).message('Title could not exceed 1000').required(),
-    content: joi.string().required(),
+    title: joi.string().max(1000).required().messages({
+        "string.base": "entry should be a string",
+        "string.max": "entry should be not exit 1000 character",
+        "string.empty": "Title is not allowed to be empty."
+    }),
+    content: joi.string().required().messages({
+        "string.base": "entry should be a string",
+        "string.empty": "Content is not allowed to be empty."
+    }),
     image: joi.any(),
-    created_on: joi.date().max('now'),
-    likes: joi.number()
+    created_on: joi.date().max('now').message('Created date should not be greater than now'),
+    likes: joi.number().integer().min(0).message('likes should be positive integer')
 })
